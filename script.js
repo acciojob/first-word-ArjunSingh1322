0da6c63d1ce22f20d0a1cc78e008fdf03e677365
ArjunSingh1322 <as3388776@gmail.com>
2025-03-19 08:51:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 function firstWord(s) {
-  // your code here
+    let arr = s.split(" "); // Split the string into an array of words
+    return arr[0]; // Return the first word
 }
 
 // Do not change the code below
-
 const s = prompt("Enter String:");
 alert(firstWord(s));
+
